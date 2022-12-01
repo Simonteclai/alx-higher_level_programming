4b55f2233db41ab79da564d9e038b8efa4e3fbbe
simonteclai <nomistec.st@gmail.com>
2022-12-01 11:22:23 -0800

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print("{} argumenta.".format(num - 1))
     else:
         print("{} arguments.".format(num - 1))
-        for i in range(1, num):
-            print("{}: {}".format(i, sys.argv[i]))
+
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
