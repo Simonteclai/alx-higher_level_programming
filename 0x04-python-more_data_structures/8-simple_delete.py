8d9767f4ce0f7c79df9e832827fe1f207f5cec09
simonteclai <nomistec.st@gmail.com>
2022-12-07 12:47:24 -0800

    9

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,4 +3,4 @@ def simple_delete(a_dictionary, key=""):
     search = str(key)
     if search in a_dictionary:
         a_dictionary.pop(search)
-        return a_dictionary
+    return a_dictionary
